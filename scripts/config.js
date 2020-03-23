99e721a1299583daf085760c52f4e69f01337f3f
raylee1994 <849103991@qq.com>
2020-03-23 23:40:02 +0800

    repair

---
+++ scripts/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dev: {
+        outputPath: "dist",
+        publicPath: "/",
+        port: 8080
+    },
+    prod: {
+        outputPath: "dist",
+        publicPath: "static"
+    }
+}
