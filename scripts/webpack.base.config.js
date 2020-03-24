d3b19895230bc17e0ee6466610db1a5fe12ae85c
raylee1994 <849103991@qq.com>
2020-03-24 23:43:15 +0800

    repair

--- scripts/webpack.base.config.js
+++ scripts/webpack.base.config.js
@@ -1,3 +1,28 @@
 var webpack = require("webpack");
 var path = require("path");
+var config = require("./config");
 
+var is_development = process.env.NODE_ENV == "development";
+
+var entry = {
+    main: path.resolve(__dirname, "../src/main.ts")
+};
+
+var output = {
+    path: path.resolve(__dirname, "../dist"),
+    filename: is_development ? "[name].js" : "[name].[chunkhash:8].js",
+    publicPath: is_development ? config.dev.publicPath : config.prod.publicPath
+};
+
+var module = {
+    rules: [
+        {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: "node_module",
+            options: {
+
+            }
+        }
+    ]
+}
