93bb518a6194e7083c989e792a9f26e8dee4be74
raylee1994 <849103991@qq.com>
2020-03-25 18:52:32 +0800

    repair

--- scripts/webpack.base.config.js
+++ scripts/webpack.base.config.js
@@ -21,7 +21,7 @@ var module = {
             loader: "ts-loader",
             exclude: "node_module",
             options: {
-
+                appendTsSuffixTo: /\.vue$/
             }
         }
     ]
